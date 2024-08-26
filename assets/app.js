dbe75288f9f5ee2023f7e94baa24e1af2cbcc553
jsdevrazuislam <razuislam.upwork@gmail.com>
2024-08-26 21:51:48 +0600

    add header footer and basic layout for home

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,31 @@
 const footerDate = document.querySelector(".footerDate");
+// const menuButton = document.querySelector(".hamburger_menu");
+// const sidebar = document.querySelector(".sidebar");
+// const overlay = document.querySelector(".overlay");
+// const close_button = document.querySelector(".close_button");
 
 document.addEventListener("DOMContentLoaded", () => {
-    footerDate.innerHTML = new Date().getFullYear();
+  footerDate.innerHTML = new Date().getFullYear();
 });
+
+// function disableScroll() {
+//   document.body.style.overflow = "hidden";
+// }
+// function enableScroll() {
+//   document.body.style.overflow = "auto";
+// }
+
+// function closeMenu() {
+//   sidebar.classList.remove("active");
+//   overlay.classList.remove("active");
+//   enableScroll();
+// }
+
+// menuButton.addEventListener("click", () => {
+//   sidebar.classList.add("active");
+//   overlay.classList.add("active");
+//   disableScroll();
+// });
+
+// close_button.addEventListener("click", closeMenu);
+// overlay.addEventListener("click", closeMenu);
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./**/*.liquid"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary:"#B88E2F",
+        fontColor:"#333333",
+        fontColorSecondary:"#666666",
+      },
+      screens: {
+        xxs: "360px",
+        xs: "390px",
+        sm: "640px",
+        md: "768px",
+        mdd: "820px",
+        "md-xl": "990px",
+        "lg-lg": "1024px",
+        lg: "1152px",
+        xl: "1280px",
+        "2xl": "1536px",
+      }
+    },
   },
   plugins: [],
 }
